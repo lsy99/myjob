dcd8bc60f8c9dd511abf23c2d0af454b34b76c1f
lsy <1154113579@qq.com>
2019-12-23 14:00:35 +1400

    new

---
+++ week15/code/my.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include<signal.h>
+#include<sys/param.h>
+#include<sys/stat.h>
+#include<time.h>
+#include<string.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<signal.h>
+#include<time.h>
+#include<sys/wait.h>
+#include<pthread.h>
+#include<sys/time.h>
+#include<semaphore.h>
+#define LOOP 10000000
+#define NUM 4
+#define TN 100
+#define WRITER_FIRST
+#define check_error(return_val,msg){\
+		if(return_val !=0){\
+			fprintf(stderr,"%s:%s\n",msg,strerrir(return_val));\
+		exit(-1);}\
+}
+
+#define err_exit(MESSAGE)( \
+	perror(MESSAGE),\
+	exit(1)\
+)
+
